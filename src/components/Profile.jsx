b91d12ee895eb360fde1a7e72309867b835b3d5a
aleksey_abdulin <themoradell@gmail.com>
2021-04-06 20:18:53 +1000

    Оформил карту в секции профиля. Осталось добавить обновление в реальном времени полей пречека

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './Header';
 import { Input } from './Input';
+import sprite from './../assets/sprite.svg'
 import '../stylesheets/profile.css';
 import '../stylesheets/button.css';
 
@@ -10,12 +11,12 @@ export const Profile = () => {
     <>
       <Header />
       <div className="profile">
-        <div className="profile__content">
+        <div className="profile__wrapper">
           <div className="profile__text">
             <div className="profile__title">Профиль</div>
             <div className="profile__description">Введите платежные данные</div>
           </div>
-          <div className="profile__card">
+          <div className="profile__content">
             <form className="profile__form" id="card">
               <Input name='name' type='text' label='Имя владельца' placeholder='Иван Иванов' />
               <Input name='card-number' type='text' label='Номер карты' placeholder='0000 0000 0000 0000' />
@@ -25,8 +26,22 @@ export const Profile = () => {
               </div>
             </form>
             <div className="profile__precheck">
-              <div className="profile__card-content">
-
+              <div className="profile__precheck-content">
+                <div className="profile__precheck-up-side">
+                  <svg className="profile__precheck-logo">
+                    <use xlinkHref={`${sprite}#logo`}></use>
+                  </svg>
+                  <div className="profile__precheck-date">01/02</div>
+                </div>
+                <div className="profile__precheck-card-number">5545 2300 3432 4521</div>
+                <div className="profile__precheck-bottom-side">
+                  <svg className="profile__precheck-chip">
+                    <use xlinkHref={`${sprite}#chip`}></use>
+                  </svg>
+                  <svg className="profile__precheck-master-card-logo">
+                    <use xlinkHref={`${sprite}#master-card`}></use>
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
